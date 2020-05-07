add3c60b58ce7dfc7f208854f276e4ec682d8495
Henrik Hjalmarsson <henrik.hjalmarsson@payex.com>
2020-05-07 21:49:05 +0200

    Removed some authentication code that didn't end up being used at all.

--- src/authentication/Hasher.js
+++
@@ -1,7 +0,0 @@
-import sha512 from "crypto-js/sha512";
-
-export class Hasher {
-    createHash(text, salt) {
-        return sha512(text + salt);
-    }
-}
--- src/authentication/UsersAccessor.js
+++
@@ -1,9 +0,0 @@
-import { firestore } from '../utils/firebase.js';
-
-export class UsersAccessor {
-    getUser(username) {
-        return firestore.collection("users")
-                        .doc(username)
-                        .get();
-    }
-}
